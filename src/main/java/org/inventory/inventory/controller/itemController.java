ac17abd1928677e95c5aede9cb7b33af3531af97
Harshith N J <njharshith122@gmail.com>
2025-02-17 17:44:16 +0530

    save a item

--- src/main/java/org/inventory/inventory/controller/itemController.java
+++ src/main/java/org/inventory/inventory/controller/itemController.java
@@ -1,7 +1,11 @@
 package org.inventory.inventory.controller;
 
+import org.inventory.inventory.dto.item;
 import org.inventory.inventory.service.itemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,10 @@ public class itemController {
     
     @Autowired
     itemService service;
+
+    // To save a item in the inventory database
+    @PostMapping("/items")
+    public ResponseEntity<Object> saveItem(@RequestBody item item){
+        return service.saveItem(item);
+    }
 }
--- src/main/java/org/inventory/inventory/service/itemService.java
+++ src/main/java/org/inventory/inventory/service/itemService.java
@@ -1,7 +1,13 @@
 package org.inventory.inventory.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.inventory.inventory.dto.item;
 import org.inventory.inventory.repository.itemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +15,26 @@ public class itemService {
     
     @Autowired
     itemRepository repository;
+
+    public ResponseEntity<Object> saveItem(item item) {
+        if(repository.existsById(item.getId())){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Item id Already exists");
+
+            return new ResponseEntity<Object>(map, HttpStatus.IM_USED);
+        }else if(item.getPrice() <= 0 || item.getQuantity() <= 0){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Price and Quantity must be greater than 0");
+
+            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
+        }else{
+            repository.save(item);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Item saved successfully");
+            map.put("item", item);
+
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
 }
